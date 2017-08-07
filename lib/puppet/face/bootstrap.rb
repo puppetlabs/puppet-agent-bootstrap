4fd1ece5d044d9884edf9f36f9e8cb510c8782dd
Adrien Thebo <git@somethingsinistral.net>
2017-08-07 14:13:00 -0700

    Add `verify` action to check for signed cert

--- lib/puppet/face/bootstrap.rb
+++ lib/puppet/face/bootstrap.rb
@@ -1,6 +1,41 @@
 require 'puppet/face'
 require 'puppet/ssl'
 
+module PuppetX
+  module Puppetlabs
+    module Bootstrap
+      def verify_signed_cert!(hostcert)
+        if !hostcert.nil?
+          Puppet.notice "Found a certificate for #{Puppet[:certname]}"
+        else
+          Puppet.error "No signed certificate found for #{Puppet[:certname]}"
+          exit(1)
+        end
+      end
+
+      def verify_ssl_files_match!(hostcert, hostkey)
+        if hostcert.content.check_private_key(hostkey.content)
+          Puppet.notice "Private key matches certificate"
+        else
+          Puppet.error "Signed certificate does not match host private key"
+          exit(1)
+        end
+      end
+
+      def verify_node_definition_reachable!
+        Puppet::Node.indirection.find(Puppet[:node_name_value],
+                                             environment: Puppet::Node::Environment.remote('production'),
+                                             ignore_cache: true,
+                                             fail_on_404: true)
+        Puppet.notice("Contacted Puppet master for node definition")
+      rescue => e
+        Puppet.err "Unable to reach Puppet master: #{e.message}"
+        exit(1)
+      end
+    end
+  end
+end
+
 Puppet::Face.define(:bootstrap, '0.1.0') do
   copyright "Puppet", 2017
   summary "Initialize the Puppet agent"
@@ -11,11 +46,28 @@ Puppet::Face.define(:bootstrap, '0.1.0') do
     when_invoked do |opts|
       Puppet::SSL::Oids.register_puppet_oids
       Puppet::SSL::Oids.load_custom_oid_file(Puppet[:trusted_oid_mapping_file])
-      Puppet::SSL::Host.ca_location = :none
-      Puppet.settings.preferred_run_mode = "agent"
-      Puppet.settings.use(:ssl)
 
       Puppet::SSL::Host.localhost
+    end
+  end
+
+  action(:verify) do
+    summary "Verify that the Puppet agent has a signed certificate"
+
+    when_invoked do |opts|
+      extend PuppetX::Puppetlabs::Bootstrap
+      hostcert = Puppet::SSL::Certificate.indirection.find(Puppet[:certname])
+      hostkey = Puppet::SSL::Key.indirection.find(Puppet[:certname])
+
+      verify_signed_cert!(hostcert)
+      verify_ssl_files_match!(hostcert, hostkey)
+      verify_node_definition_reachable!
+
+      hostcert
+    end
+
+    when_rendering :console do |cert|
+      cert.content.to_s if cert
     end
   end
 end
