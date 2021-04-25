fa924be87427518d35b46945895984683529f1a1
Sean Kumar <skumar@triplelift.com>
2021-04-25 10:14:34 -0400

    Fix user-data bootstrapping on AWS Opsworks with Puppet Enterprise (v2019.8.5) and Puppet (6.21.1)
    Resolves this error: Error: Cannot create CSR with extension request pp_role: OBJ_txt2obj: first num too large

--- lib/puppet/application/bootstrap.rb
+++ lib/puppet/application/bootstrap.rb
@@ -1,4 +1,5 @@
 require 'puppet/application/face_base'
+require 'puppet/ssl/oids'
 
 class Puppet::Application::Bootstrap < Puppet::Application::FaceBase
   def app_defaults
@@ -13,6 +14,7 @@ class Puppet::Application::Bootstrap < Puppet::Application::FaceBase
   def setup
     super
 
+    Puppet::SSL::Oids.register_puppet_oids
     Puppet::SSL::Host.ca_location = :none if Gem::Version.new(Puppet.version) < Gem::Version.new('6.0')
     Puppet.settings.preferred_run_mode = "agent"
     Puppet.settings.use(:ssl)
